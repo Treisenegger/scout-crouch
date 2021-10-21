32f94f4e22a18731d6a3166a6a56fc318bb427ae
Thomas Reisenegger <thomas.reiseneggerb@gmail.com>
2021-10-21 19:08:39 -0300

    Draw vision cone mesh

--- Scout and Crouch/Assets/Scripts/EnemyVision.cs
+++ Scout and Crouch/Assets/Scripts/EnemyVision.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 // TODO: Move rotation/movement to an enemy movement controller
 public class EnemyVision : MonoBehaviour {
+
+    struct ViewCastInfo {
+        public bool hit;
+        public Vector3 point;
+        public float distance;
+        public float angle;
+
+        public ViewCastInfo(bool _hit, Vector3 _point, float _distance, float _angle) {
+            hit = _hit;
+            point = _point;
+            distance = _distance;
+            angle = _angle;
+        }
+    }
+
     public float visionRange = 2f;
     [Range(0f, 360f)]
     public float visionAngle = 45f;
@@ -14,12 +29,18 @@ public class EnemyVision : MonoBehaviour {
     [SerializeField] float rotationSpeed = 45f;
     [SerializeField] float targetDetectionFreq = 0.2f;
     [SerializeField] int visionResolution = 10;
+    [SerializeField] MeshFilter visionConeMeshFilter;
+    Mesh visionConeMesh;
 
     private void Start() {
+        visionConeMesh = new Mesh();
+        visionConeMesh.name = "Vision Cone Mesh";
+        visionConeMeshFilter.mesh = visionConeMesh;
+
         StartCoroutine(InitiatePlayerDetection());
     }
 
-    private void Update() {
+    private void LateUpdate() {
         DrawVisionCone();
     }
 
@@ -62,14 +83,48 @@ public class EnemyVision : MonoBehaviour {
         }
     }
 
+    private ViewCastInfo ViewCast(float _globalAngle) {
+        Vector3 _dir = DirFromAngle(_globalAngle, true);
+        RaycastHit _hit;
+
+        bool _collided = Physics.Raycast(transform.position, _dir, out _hit, visionRange, obstacleMask);
+        if (_collided) {
+            return new ViewCastInfo(true, _hit.point, _hit.distance, _globalAngle);
+        }
+        else {
+            return new ViewCastInfo(false, transform.position + _dir * visionRange, visionRange, _globalAngle);
+        }
+    }
+
     private void DrawVisionCone() {
         float _stepAngleSize = visionAngle / visionResolution;
+        List<Vector3> _viewPoints = new List<Vector3>();
 
         for (int i = 0; i < visionResolution + 1; i++) {
             float _angle = transform.eulerAngles.y - (visionAngle / 2) + i * _stepAngleSize;
-            Color _color = isAlerted ? Color.red : Color.white;
-            Debug.DrawLine(transform.position, transform.position + DirFromAngle(_angle, true) * visionRange, _color);
+            ViewCastInfo newViewCast = ViewCast(_angle);
+            _viewPoints.Add(newViewCast.point);
         }
+
+        int _vertexCount = _viewPoints.Count + 1;
+        Vector3[] _vertices = new Vector3[_vertexCount];
+        int[] _triangles = new int[(_vertexCount - 2) * 3];
+        _vertices[0] = Vector3.zero;
+
+        for (int i = 0; i < _vertexCount - 1; i++) {
+            _vertices[i + 1] = transform.InverseTransformPoint(_viewPoints[i]);
+
+            if (i < _vertexCount - 2) {
+                _triangles[3*i] = 0;
+                _triangles[3*i + 1] = i + 1;
+                _triangles[3*i + 2] = i + 2;
+            }
+        }
+
+        visionConeMesh.Clear();
+        visionConeMesh.vertices = _vertices;
+        visionConeMesh.triangles = _triangles;
+        visionConeMesh.RecalculateNormals();
     }
 
     public Vector3 DirFromAngle(float _angleInDegrees, bool _angleIsGlobal) {
